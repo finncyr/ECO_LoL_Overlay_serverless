42f8751795f4d541da6a872d6e63b4e00d6e8f8b
Restyled.io <commits@restyled.io>
2021-01-22 14:41:41 +0000

    Restyled by prettier

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ $("h1").editable();
 
 $("#slideshow > div:gt(0)").hide();
 
-setInterval(function() {
-  $('#slideshow > div:first')
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo('#slideshow');
+setInterval(function () {
+  $("#slideshow > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo("#slideshow");
 }, 9000);
